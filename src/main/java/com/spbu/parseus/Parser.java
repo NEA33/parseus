52475672d79930e6d386f017d9d54224930d9356
SingularityA <singularity.m101@gmail.com>
2019-10-07 22:53:56 +0300

    Replaced java interface with kotlin interface

--- src/main/java/com/spbu/parseus/Parser.java
+++
@@ -1,8 +0,0 @@
-package com.spbu.parseus;
-
-import java.util.List;
-
-public interface Parser {
-    String getText();
-    List<String> getLinks();
-}
---
+++ src/main/kotlin/com/spbu/parseus/Parser.kt
@@ -0,0 +1,6 @@
+package com.spbu.parseus
+
+interface Parser {
+    val text: String
+    val links: List<String>
+}
